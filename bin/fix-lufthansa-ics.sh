4ad12074ad7afa10fe4bb71a393d0f4be13f333d
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-03-04 14:18:02 +0100

    usage (-h, --help)

--- bin/fix-lufthansa-ics.sh
+++ bin/fix-lufthansa-ics.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 FILE="$1"
+NAME=$(basename $0)
 
+if [ "$1" = "-h" ] ||  [ "$1" = "--help" ]
+then
+    echo "NAME"
+    echo "  $NAME - fixes Lufthansa's broken ics files"
+    echo ""
+    echo "SYNOPSIS"
+    echo "  $NAME [FILE]"
+    echo ""
+    echo "DESCRIPTION"
+    echo "  Fixes Lufthansa's broken ics files by trimming DSTART and DTEND"
+    echo "  tags as well as remove blank lines"
+    echo ""
+    echo "  If no file is specified stdin is used."
+    echo "  $NAME copies resulting (fixed) ics to clipboard"
+    echo "  as well as write to a file (for import)"
+    echo ""
+    exit 0
+fi
+   
 if [ ! -f $FILE ]
 then
     echo "File \"$FILE\" does not exist"
