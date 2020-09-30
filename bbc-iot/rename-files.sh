b0ef8f24d628e2487f31997852e5dd6761927cbe
Sandklef Henrik <henrik.sandklef@kdab.com>
2020-10-01 00:02:05 +0200

    first rev - rename bbc files after year etc

---
+++ bbc-iot/rename-files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in $(ls *.mp3);
+do
+    YEAR=$(id3v2 --list $i | grep Date | cut -d : -f 2);
+    if [ "$YEAR" = "" ] ;
+    then
+        YEAR=$(id3v2 --list $i | grep Year | grep -v -e Album -e songname | cut -d : -f 2);
+    fi ;
+
+    TITLE=$(id3v2 --list $i | grep TIT2 | cut -d : -f 3 | sed 's, ,_,'g);
+    if [ "$TITLE" = "" ] ;
+    then
+        TITLE=$(id3v2 --list $i | grep TIT2 | cut -d : -f 2 | sed -e 's,^ ,,g' -e 's, ,_,'g) ;
+    fi;
+
+    echo mv $i $YEAR-$TITLE.mp3;
+done  
