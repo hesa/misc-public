08f502c7617d6495c52397778b480d514ef92c6b
Sandklef Henrik <hesa@sandklef.com>
2022-03-16 19:21:05 +0100

    Add "

--- bin/time.sh
+++ bin/time.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+if [ "$1" != "" ]
+then
+    REMAINING=$1
+fi
+
 while (true);
 do
     clear
-    banner $(date "+%H:%M:%S")
+    banner "$(date '+%H %M %S')"
     sleep 1
+    if [ "${REMAINING}" != "" ]
+    then
+        REMAINING=$(( REMAINING - 1 ))
+        if [ $REMAINING -lt 1 ]
+        then
+            break
+        fi
+    fi
+    
 done
